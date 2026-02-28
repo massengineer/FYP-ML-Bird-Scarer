a90c328daaf7e260746dbf27ce426c5296524145
Louis Massingham <143968432+massengineer@users.noreply.github.com>
2026-02-28 14:03:47 +0000

    feat/esp32-acousto-mechanics: This code is used on an exterior esp-WROOM-32 board to play audio through a speaker driven by a class D amplifier and a servo motor.

---
+++ hawk_screech_and_servo_scarer.ino
@@ -0,0 +1,58 @@
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+#include <ESP32Servo.h>
+
+I2SStream i2s;
+BluetoothA2DPSink a2dp_sink(i2s);
+
+Servo myservo;
+int servoPin = 4;
+
+// Use a "volatile" flag so both threads can see it
+volatile bool triggerServo = false;
+
+void audio_state_changed(esp_a2d_audio_state_t state, void *ptr) {
+  // We only set the flag here. No delays, no heavy logic!
+  if (state == ESP_A2D_AUDIO_STATE_STARTED) {
+    triggerServo = true; 
+  }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    auto cfg = i2s.defaultConfig();
+    cfg.pin_bck = 26;
+    cfg.pin_ws = 25;
+    cfg.pin_data = 22;
+    i2s.begin(cfg);
+
+    myservo.setPeriodHertz(50);
+//    // Initial test movement
+//    myservo.attach(servoPin);
+//    myservo.write(0); 
+//    delay(500);
+//    myservo.detach();
+  
+    // Register the callback
+    a2dp_sink.set_on_audio_state_changed(audio_state_changed);
+    a2dp_sink.start("Hawk_Speaker");
+}
+
+void loop() {
+  // Check if the Bluetooth callback set the flag to true
+  if (triggerServo) {
+
+    for (int i = 0; i < 3; i++) {
+      myservo.attach(servoPin);
+      myservo.write(90); 
+      delay(500);
+      myservo.write(0);  
+      delay(500);
+      myservo.detach();
+      }
+    
+    
+    triggerServo = false; // Reset the flag
+  }
+}
